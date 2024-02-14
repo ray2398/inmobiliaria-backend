52191f9aa0abcd3b6416aafad51e27d5c217b069
ray2398 <raymundoolarte@gmail.com>
2024-02-14 03:15:25 -0600

    register, login, logout

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    //Función que utilizaremos para registrar al usuario
+    public function register(Request $request) {
+        $data = $request->all();
+        //Validaciones
+        $validator = Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email|max:255',
+            'password' => 'required|string|min:8|confirmed',
+            'telefono' => 'required',
+        ]);
+
+        //Devolvemos un error de validación en caso de fallo en las verificaciones
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->messages()], 422);
+        }
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'telefono' => $data['telefono'],
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $cookie = cookie('token', $token, 60 * 24); // 1 day
+
+        return response()->json([
+            'user' => $user,
+        ])->withCookie($cookie);
+    }
+
+    //Funcion que utilizaremos para hacer login
+    public function login(Request $request) {
+        //Indicamos que solo queremos recibir email y password de la request
+        $credentials = $request->only('email', 'password');
+
+        //Validaciones
+        $validator = Validator::make($credentials, [
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        //Devolvemos un error de validación en caso de fallo en las verificaciones
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->messages()], 422);
+        }
+
+        $user = User::where('email', $credentials['email'])->first();
+
+        if (!$user || !Hash::check($credentials['password'], $user->password)) {
+            return response()->json([
+                'message' => 'El correo electrónico o la contraseña son incorrectos'
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $cookie = cookie('token', $token, 60 * 24); // 1 day
+
+        return response()->json([
+            'user' => $user,
+        ])->withCookie($cookie);
+    }
+
+     //Función que utilizaremos para eliminar el token y desconectar al usuario
+    public function logout(Request $request) {
+        $request->user()->currentAccessToken()->delete();
+
+        $cookie = cookie()->forget('token');
+
+        return response()->json([
+            'message' => 'Logged out successfully!'
+        ])->withCookie($cookie);
+    }
+}
