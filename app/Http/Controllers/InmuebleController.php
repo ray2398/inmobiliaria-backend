722233320de381e0179b14c554c6e98fc6c93f27
ray2398 <raymundoolarte@gmail.com>
2024-02-14 01:00:42 -0600

    Init Project

---
+++ app/Http/Controllers/InmuebleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Inmueble;
+use Illuminate\Http\Request;
+
+class InmuebleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Inmueble  $inmueble
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Inmueble $inmueble)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Inmueble  $inmueble
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Inmueble $inmueble)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Inmueble  $inmueble
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Inmueble $inmueble)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Inmueble  $inmueble
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Inmueble $inmueble)
+    {
+        //
+    }
+}
